145cd468fa4ac86afdb9cc015a69f079371d7dcb
alexander1fein <feinberg.alexander@gmail.com>
2022-09-07 12:56:06 -0400

    Done

--- server/app.js
+++ server/app.js
@@ -5,19 +5,22 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
+app.use((req, res, next)=>{
+  const newErr = new Error("Sorry, the requested resource couldn't be found")
+  newErr.errorCode = 404
+  // throw newErr
+  next(newErr)
+})
 
-const notFound = (err, req, res, next)=>{
+
+//Handle Error
+app.use((err, req, res, next)=>{
   const errorCode = err.errorCode
   res.status(errorCode || 500)
   console.error(`NEW ERROR: ${err}, STATUS: ${errorCode}`)
   const body = {"message": err.message, "statusCode":errorCode}
   res.json(body)
-}
-app.use('/',[(req, res, next)=>{
-  const newErr = new Error("Sorry, the requested resource couldn't be found")
-  newErr.errorCode = 404
-  next(newErr)
-}, notFound])
+})
 
 
 const port = 5000;
